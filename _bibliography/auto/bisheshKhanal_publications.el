b27cd2a1a733adf8d77f1ae00601b1153c9685d6
Bishesh Khanal <bisheshkh@gmail.com>
2016-06-16 15:24:49 +0200

    Initialize basic skeleton

---
+++ _bibliography/auto/bisheshKhanal_publications.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "bisheshKhanal_publications"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Khanal_2016b"
+    "Khanal_2016a"
+    "Khanal_2015"
+    "Khanal_2014"
+    "Khanal_2012"
+    "Khanal_2011"))
+ :bibtex)
+
